14943592a0c39e2ad5bd100ef1a0792b3ba2acbe
Ashok Satyanarayana <ashok.satyanarayana@gmail.com>
2018-05-23 16:52:34 +0000

    fixing testcase

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -6,7 +6,14 @@ class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
-        result = index.handler(None, None)
+        result = index.handler(
+            {"headers":{
+                "Request URL":" https",
+                "X-GitHub-Delivery":" b08e6ce8-5ea5-11e8-8607-2301863fde29",
+                "X-GitHub-Event":" push",
+                "X-Hub-Signature":" sha1=c40279d448a496d073e20ef2686bd26d3e87d5af"
+             }   
+            }, None)
         print(result)
         #self.assertEqual(result['statusCode'], 200)
         #self.assertEqual(result['headers']['Content-Type'], 'application/json')
