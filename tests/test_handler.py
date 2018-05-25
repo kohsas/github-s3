8c3a3b46554cbb3e8b40b0ff8458cad9886b5e6c
Ashok Satyanarayana <ashok.satyanarayana@gmail.com>
2018-05-25 16:28:42 +0000

    fixed test case

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -13,7 +13,11 @@ class TestHandlerCase(unittest.TestCase):
                 "X-GitHub-Event":" push",
                 "X-Hub-Signature":" sha1=c40279d448a496d073e20ef2686bd26d3e87d5af"
              },
-             "body" : {"t":1}
+             "body" : {"repository":{
+                             "id":134572202,
+                             "name":"www.sangraha.co.in.deploy"
+                             }
+             }
             }, None)
         print(result)
         #self.assertEqual(result['statusCode'], 200)
